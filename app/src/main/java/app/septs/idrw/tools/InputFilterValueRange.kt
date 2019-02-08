b6054de844565a3da7a31468742ac6087d7af0d8
Septs <github@septs.pw>
2019-02-08 12:26:32 +0800

    rewrite InputFilterValueRange

--- app/src/main/java/app/septs/idrw/tools/InputFilterValueRange.kt
+++ app/src/main/java/app/septs/idrw/tools/InputFilterValueRange.kt
@@ -4,26 +4,12 @@ import android.text.InputFilter
 import android.text.Spanned
 
 class InputFilterValueRange(private var range: LongRange) : InputFilter {
-    override fun filter(
-            source: CharSequence,
-            start: Int,
-            end: Int,
-            dest: Spanned,
-            dstart: Int,
-            dend: Int
-    ): CharSequence? {
-        try {
-            val input = (dest.toString() + source.toString()).toLong()
-            if (isInRange(range.first, range.last, input)) {
-                return null
-            }
-        } catch (e: NumberFormatException) {
-            // ignore exception
-        }
-        return ""
+    override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dstart: Int, dend: Int): CharSequence? {
+        val input = (dest.toString() + source.toString()).toLongOrNull()
+        val disallow = input != null && isInRange(range.first, range.last, input)
+        return if (disallow) null else ""
     }
 
-    private fun isInRange(a: Long, b: Long, c: Long): Boolean {
-        return if (b > a) c in a..b else c in b..a
-    }
+    private fun isInRange(a: Long, b: Long, target: Long) =
+            if (b > a) target in a..b else target in b..a
 }
