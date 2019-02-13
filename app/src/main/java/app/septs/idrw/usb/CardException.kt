b2b4b5c753d84bbcc23ec185bd9ff0e5c3068dbd
Septs <github@septs.pw>
2019-02-13 19:18:35 +0800

    remove 0x80 error code

--- app/src/main/java/app/septs/idrw/usb/CardException.kt
+++ app/src/main/java/app/septs/idrw/usb/CardException.kt
@@ -3,7 +3,6 @@ package app.septs.idrw.usb
 class CardException(private val code: Byte) : Exception() {
     override val message: String?
         get() = when (code.toInt()) {
-            0x80 -> "Set successfully"
             0x81 -> "Set failed"
             0x82 -> "Communication timeout"
             0x83 -> "Card not found"
